1a0192743c5d58d900b3771d03bd1173361812ac
Yoriyuki Yamagata <yoriyuki.y@gmail.com>
2019-01-21 15:55:55 +0900

    remove act_denormalize

--- act_denormalize.m
+++
@@ -1,8 +0,0 @@
-function [action] = act_denormalize(config, action_normalized)
-    action_normalized = min([1.0 1.0], max([-1.0 -1.0], action_normalized));
-    lower = config.input_range(:,1)';
-    upper = config.input_range(:,2)';
-    middle = (lower + upper)/2.0;
-    action = (action_normalized .* (upper - middle) + middle);
-end
-
