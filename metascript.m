4ad7ae463adddb8c85576912ebd65231ad1e78f9
Yoriyuki Yamagata <yoriyuki.yamagata@aist.go.jp>
2019-02-01 18:36:23 +0900

    Revert "Revert "Revert "Revert "PTC experiment""""
    This reverts commit 6b32a7d4fc098942bb7c9b1a2a4a3451c0fcb138.

--- metascript.m
+++ metascript.m
@@ -4,12 +4,12 @@
 % Configurations
 %%%%%%%%%%%%%%%%
 global workers_num logDir;
-workers_num = 1;
+workers_num = 10;
 staliro_dir = '../s-taliro_public/trunk/';
 breach_dir = '../breach';
 logDir = '../falsify-data/';
-maxIter = 1;
-maxEpisodes = 20;
+maxIter = 20;
+maxEpisodes = 200;
 do_arch2014 = false;
 do_ptc = true;
 do_insulin = false;
